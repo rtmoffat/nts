00a0a6507de334d24814c2bc2e2e17580c9da814
Ralph T. Moffat <rtmoffat@gmail.com>
2023-05-21 00:44:37 +0000

    updated env, added cors

--- routes/index.js
+++ routes/index.js
@@ -17,19 +17,25 @@ connection.on('error', function(err) {
   console.log(err.code); // 'ER_BAD_DB_ERROR'
 });
 
+const cors = require('cors');
+router.use(cors({
+    origin: '*'
+}));
+
 router.all('/:restaurants(restaurants)/:restaurantId?/:tables(tables)?/:tableId?', (req,res,next) => {
 
 		query='select * from restaurants;';
-		query='select * from restaurants where id=?;';
-		query='insert into restaurants (Name) values(?);';
-		connection.query(query, ['Fox'],(error, results, fields) => {
+		//query='select * from restaurants where id=?;';
+		//query='insert into restaurants (Name) values(?);';
+		connection.query(query,(error, results, fields) => {
 			console.log('Results: ',results);
 			res.send(results);
 		});
 });
 
 router.get('/',(req,res,next) => {
-	res.send('TS Coming soon!');
+	console.log(req.query);
+	res.send('TS Coming Soon!' + req.query);
 });
 
 module.exports = router;
